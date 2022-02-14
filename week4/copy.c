cb6ca4debee44e71b415bd521e15b0941fc84eca
xiaoyumibing <89766529+xiaoyumibing@users.noreply.github.com>
2022-02-14 16:22:54 +0800

    Create copy.c

---
+++ week4/copy.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s = get_string("s:");
+
+    char *t = malloc(strlen(s) + 1);
+
+    // NUL is "/0",the last of string
+    // NULL represent nul pointer
+    if (t == NULL)
+    {
+        return 1;
+    }
+
+    // i<=n , for the last character ,null
+    for (int i = 0, n = strlen(s); i < n + 1; i++)
+    {
+        t[i] = s[i];
+    }
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+    free(t);
+}
